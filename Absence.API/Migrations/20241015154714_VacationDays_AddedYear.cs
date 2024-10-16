66166c4eca966e5d5e69be4bf71b7e770906be35
deeweade <olegpetrow16@gmail.com>
2024-10-16 12:42:31 +0300

    changed migration

--- Absence.API/Migrations/20241015154714_VacationDays_AddedYear.cs
+++ Absence.API/Migrations/20241015154714_VacationDays_AddedYear.cs
@@ -17,45 +17,11 @@ namespace Absence.API.Migrations
                 type: "int",
                 nullable: false,
                 defaultValue: 0);
-
-            migrationBuilder.CreateTable(
-                name: "Position_And_Employees_On_Day",
-                columns: table => new
-                {
-                    DATE_FIELD = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    TERMINATION_DATE = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    SID = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    SNAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    SOPERCENT = table.Column<decimal>(type: "decimal(18,0)", precision: 18, scale: 0, nullable: true),
-                    SPPERCENT = table.Column<decimal>(type: "decimal(18,0)", precision: 18, scale: 0, nullable: true),
-                    PID = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PGRADE = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PGRADESTARTDATE = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    CITY_NAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    HIRED_DATE = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    OFFICENAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    CATEGORYNAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    CCID = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    OID = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_4 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_5 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_6 = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LEVEL_7 = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Position_And_Employees_On_Day");
-
             migrationBuilder.DropColumn(
                 name: "Year",
                 table: "VacationDays");
